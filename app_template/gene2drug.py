5bf16dda3ebd259f512c2aa7233f85f320082147
Lukas_Fallmann <lukas.fallmann27@gmail.com>
2023-06-06 09:21:53 +0200

    add gene2drug

---
+++ app_template/gene2drug.py
@@ -0,0 +1,26 @@
+import logging
+from pathlib import Path
+
+import pandas as pd
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+_log = logging.getLogger(__name__)
+gene2drug_router = APIRouter(tags=["Graph"])
+
+graph_data = pd.read_csv(Path(__file__).parent / "data/gwas_gene-diseases.csv.gz", compression="gzip")
+
+
+
+class DiseaseResponse(BaseModel):
+    gene: str
+    padj: float
+    disease: str
+
+
+@gene2drug_router.get("/gene2drug")
+def gene2drug(gene: str | None = None, limit: int = 1000) -> list[DiseaseResponse]:
+    df = graph_data[graph_data["disease"].str.contains("CHEBI")]
+    if gene:
+        df = graph_data[graph_data["gene"]==gene]
+    return df.head(limit).to_dict(orient="records")  # type: ignore
