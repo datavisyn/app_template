f2546159e7944f347f54a123ca66490e3ff3481f
nikolimaj <maja.nikolic1706@gmail.com>
2023-06-12 16:18:23 +0200

    temp use genenode

--- src/GeneGraph.tsx
+++ src/GeneGraph.tsx
@@ -1,101 +1,95 @@
 import React, { useState, useEffect, useRef } from 'react';
 import dagre from 'dagre';
-import { useAutocomplete,useGene2Drugs, useGene2Genes } from './store/store';
-import { ReactFlow, Background, Controls, MiniMap } from 'reactflow';
+import { useAutocomplete, useGene2Drugs, useGene2Genes } from './store/store';
+import { ReactFlow, Background, Controls, MiniMap, useNodesState, useEdgesState } from 'reactflow';
 import 'reactflow/dist/style.css';
-import cytoscape from 'cytoscape';
 import GeneNode from './GeneNode';
-import { GraphResponse } from './store/generatedAppApi';
+import DrugNode from './DrugNode';
+import DiseaseNode from './DiseaseNode';
+import { Gene2GenesApiAppGene2GenesGetApiResponse } from './store/generatedAppApi';
 
 
 const nodeWidth = 172;
 const nodeHeight = 36;
+
+const nodeTypes = { diseaseNode: DiseaseNode, geneNode: GeneNode, drugNode: DrugNode };
 
 const dagreGraph = new dagre.graphlib.Graph();
 dagreGraph.setDefaultEdgeLabel(() => ({}));
 
 const getLayoutedElements = (nodes, edges) => {
 
-    dagreGraph.setGraph({ rankdir: 'TB',align: 'UL' })
+  dagreGraph.setGraph({ rankdir: 'TB', align: 'UL' })
 
-    nodes?.forEach((node) => {
-      dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
-    });
+  nodes?.forEach((node) => {
+    dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
+  });
 
-    edges?.forEach((edge) => {
-      dagreGraph.setEdge(edge.source, edge.target);
-    });
+  edges?.forEach((edge) => {
+    dagreGraph.setEdge(edge.source, edge.target);
+  });
 
-    dagre?.layout(dagreGraph);
+  dagre?.layout(dagreGraph);
 
-    nodes?.forEach((node) => {
+  nodes?.forEach((node) => {
 
-      const nodeWithPosition = dagreGraph.node(node.id);
+    const nodeWithPosition = dagreGraph.node(node.id);
 
-      node.targetPosition = 'top';
-      node.sourcePosition = 'bottom';
+    node.targetPosition = 'top';
+    node.sourcePosition = 'bottom';
 
-      // We are shifting the dagre node position (anchor=center center) to the top left
-      // so it matches the React Flow node anchor point (top left).
-      node.position = {
-        x: nodeWithPosition.x - nodeWidth / 2,
-        y: nodeWithPosition.y - nodeHeight / 2,
-      };
+    // We are shifting the dagre node position (anchor=center center) to the top left
+    // so it matches the React Flow node anchor point (top left).
+    node.position = {
+      x: nodeWithPosition.x - nodeWidth / 2,
+      y: nodeWithPosition.y - nodeHeight / 2,
+    };
 
-      return node;
-    });
+    return node;
+  });
 
-    return { nodes, edges };
+  return { nodes, edges };
 };
 
-
-
-let x1 = 0;
-let y1 = 0;
-
-
-
-type GeneGraphProps={
-    geneID:string
+type GeneGraphProps = {
+  genes: Gene2GenesApiAppGene2GenesGetApiResponse
 }
 
 
+export function GeneGraph(props: GeneGraphProps) {
 
+  // add nodes
+  const nodes = props.genes?.map(node => ({
+    id: node.ENSG_B,
+    position: {
 
-export function GeneGraph(props:GeneGraphProps) {
+      x: Math.random() * 700,
 
-    const { data: graph } = useGene2Genes({
-        gene: props.geneID || undefined,
-        limit: 1000,
-      });
-    
-    
-      // add nodes
-      const nodes = graph?.map(node => ({
-        id: node.ENSG_B,
-        position: {
-            x: Math.random() * 700,
-            y: Math.random() * 700,},
-        data: {label: node.ENSG_B_name}
-      }))
-    
-    
-      // TODO originNodeId = parameter of function
-      const edges = graph?.map(edge => ({
-        id: edge.ENSG_A + "-" + edge.ENSG_B,
-        source: edge.ENSG_A,
-        target: edge.ENSG_B
-      }))
-    
-      const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
-        nodes,
-        edges
-      );
+      y: Math.random() * 700,
+    },
 
+    data: { label: node.ENSG_B_name }
+
+  }))
+
+  // TODO originNodeId = parameter of function
+  const edges = props.genes?.map(edge => ({
+    id: edge.ENSG_A + "-" + edge.ENSG_B,
+    source: edge.ENSG_A,
+    target: edge.ENSG_B
+  }))
+
+  const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
+    nodes,
+    edges
+  );
+
+  const [nodesForFlow, setNodes, onNodesChange] = useNodesState(layoutedNodes);
+  const [edgesForFlow, setEdges, onEdgesChange] = useEdgesState(layoutedEdges);
 
   return (
     <div style={{ height: '100%' }}>
-      <ReactFlow nodes={layoutedNodes} edges={layoutedEdges} > 
+      <ReactFlow nodes={nodesForFlow} edges={edgesForFlow} nodeTypes={nodeTypes} onNodesChange={onNodesChange} onEdgesChange={onEdgesChange}>
         <Background />
         <Controls />
       </ReactFlow>
