7d6a05a63be206bff52ecc72c477b480a41a04ec
0Falli0 <s2110458009@students.fh-hagenberg.at>
2023-10-17 11:32:00 +0200

    add edges to gene2all

--- app_template/gene2all_Implementation.py
+++ app_template/gene2all_Implementation.py
@@ -6,14 +6,39 @@ BASE_PATH = Path(__file__).parent
 
 # add file globals
 gene_names = pd.read_csv(BASE_PATH / "data/gwas_nodes.csv.gz", compression="gzip")
-graph_data = pd.read_csv( BASE_PATH / "data/STRINGv11_OTAR281119_FILTER_combined.csv.gz", compression="gzip")
-trait_data = pd.read_csv(BASE_PATH / "data/gwas_gene-diseases.csv.gz", compression="gzip")
+graph_data = pd.read_csv(
+    BASE_PATH / "data/STRINGv11_OTAR281119_FILTER_combined.csv.gz", compression="gzip"
+)
+trait_data = pd.read_csv(
+    BASE_PATH / "data/gwas_gene-diseases.csv.gz", compression="gzip"
+)
 
 
 def gene2all(gene: str | None = None, limit: int = 1000):
+    # get gene2gene edges
     df = graph_data
+    d = []
+
     if gene:
         df = df[(df["ENSG_A"] == gene) & (df["ENSG_B"] != gene)]
-    print(df.head(1).to_dict(orient="records"))
+        df.rename(columns={"ENSG_A": "source", "ENSG_B": "target"}, inplace=True)
+        df = df.drop("combined_score", axis=1)
+        d = df.head(limit).to_dict(orient="records")
 
-gene2all("ENSG00000186628")
+        # get traits
+        df = trait_data
+        df = df[(df["gene"] == gene)]
+        df.rename(columns={"gene": "source", "disease": "target"}, inplace=True)
+        df = df.drop("padj", axis=1)
+        t = df.head(limit).to_dict(orient="records")
+
+        for ele in t:
+            d.append(ele)
+
+        # add id column
+        for ele in d:
+            ele["id"] = ele["source"] + "-" + ele["target"]
+            print(ele)
+
+
+gene2all("ENSG00000030110")
