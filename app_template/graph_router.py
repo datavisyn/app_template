d497e6d51f9f7e07ee2f2e85cf1a77e4915b5fc7
0Falli0 <s2110458009@students.fh-hagenberg.at>
2023-10-24 12:30:56 +0200

    fix autocomplete and clean-up graph router

--- app_template/graph_router.py
+++ app_template/graph_router.py
@@ -1,6 +1,7 @@
 import logging
 from pathlib import Path
 
+import math
 import networkx as nx
 import numpy as np
 import pandas as pd
@@ -12,124 +13,6 @@ from pydantic import BaseModel, typing
 _log = logging.getLogger(__name__)
 graph_router = APIRouter(tags=["Graph"])
 
-BASE_PATH = Path(__file__).parent
-gene_names = pd.read_csv(BASE_PATH / "data/gwas_nodes.csv.gz", compression="gzip")
-
-
-
-
-# might not be needed if we get the name from gene_list.json / gene_descriptions.json
-def getGeneName(ensg: str):
-    entry = gene_names[gene_names["ENSG"] == ensg]
-    if entry.empty:
-        return "Gene not found"
-    return entry.iloc[0, 2]
-
-
-# might not be needed if we get the name from gene_list.json / gene_descriptions.json
-def setGeneNamesFromGeneDf(df: pd.DataFrame, isTraitResponse=False):
-    ensg = df.iloc[0, 0]
-    # if isTraitResponse:
-    #     df["gene_name"] = getGeneName(ensg)
-    # else:
-    #     df["ENSG_A_name"] = getGeneName(ensg)
-
-    df["ENSG_B_name"] = df["ENSG_B"].apply(getGeneName)
-    return df
-
-
-gene_list = (
-    pd.read_json("app_template/data/gene_list.json")
-    .drop(columns=["hgncId", "hgncSymbol"], axis=1)
-    .rename(columns={"ensemblid": "id"})
-)
-gene_details = (
-    pd.read_json("app_template/data/gene_descriptions.json")
-    .drop(columns=["fullname"], axis=1)
-    .rename(columns={"entrezGeneId": "entrezId"})
-)
-gene_data = pd.merge(gene_list, gene_details, on="entrezId")
-gene_data["type"] = "gene"
-
-trait_data = (
-    pd.read_csv(BASE_PATH / "data/gwas_gene-diseases.csv.gz", compression="gzip")
-    .drop(columns=["gene", "padj"])
-    .rename(columns={"disease": "id"})
-)
-trait_data["type"] = "disease"
-
-# TODO get trait names via API calls
-drug_data = trait_data[trait_data["id"].str.contains("CHEBI")].copy()
-drug_data["type"] = "drug"
-disease_data = trait_data[~trait_data["id"].str.contains("CHEBI")].copy()
-
-nodes = pd.concat([gene_data, disease_data, drug_data], axis=0, ignore_index=True)
-
-BASE_PATH = Path(__file__).parent
-
-# add file globals
-gene_names = pd.read_csv(BASE_PATH / "data/gwas_nodes.csv.gz", compression="gzip")
-graph_data = pd.read_csv(
-    BASE_PATH / "data/STRINGv11_OTAR281119_FILTER_combined.csv.gz", compression="gzip"
-)
-trait_data = pd.read_csv(
-    BASE_PATH / "data/gwas_gene-diseases.csv.gz", compression="gzip"
-)
-
-gene_list = (
-    pd.read_json("app_template/data/gene_list.json")
-    .drop(columns=["hgncId", "hgncSymbol"], axis=1)
-    .rename(columns={"ensemblid": "id"})
-)
-gene_details = (
-    pd.read_json("app_template/data/gene_descriptions.json")
-    .drop(columns=["fullname"], axis=1)
-    .rename(columns={"entrezGeneId": "entrezId"})
-)
-gene_nodes = pd.merge(gene_list, gene_details, on="entrezId")
-gene_nodes["type"] = "gene"
-
-trait_nodes = (
-    pd.read_csv(BASE_PATH / "data/gwas_gene-diseases.csv.gz", compression="gzip")
-    .drop(columns=["gene", "padj"])
-    .rename(columns={"disease": "id"})
-)
-trait_nodes["type"] = "disease"
-
-# TODO get trait names via API calls
-drug_nodes = trait_nodes[trait_nodes["id"].str.contains("CHEBI")].copy()
-drug_nodes["type"] = "drug"
-disease_nodes = trait_nodes[~trait_nodes["id"].str.contains("CHEBI")].copy()
-drug_nodes["synonyms"] = np.empty((len(drug_nodes), 0)).tolist()
-disease_nodes["synonyms"] = np.empty((len(disease_nodes), 0)).tolist()
-
-
-allNodes = pd.concat([gene_nodes, disease_nodes, drug_nodes], axis=0, ignore_index=True)
-
-
-@graph_router.get("/autocomplete")
-def autocomplete(search: str, limit: int | None = 10) -> list[str]:
-    full_data = (
-        pd.concat([graph_data["ENSG_A"], trait_data["disease"]]).unique().tolist()
-    )
-    return [s for s in full_data if search.lower() in s.lower()][:limit]
-
-
-class GeneResponse(BaseModel):
-    ENSG_A: str
-    ENSG_B: str
-    combined_score: float
-    ENSG_A_name: str
-    ENSG_B_name: str
-
-
-class TraitResponse(BaseModel):
-    gene: str
-    padj: float
-    disease: str
-    gene_name: str
-
-
 class Edge(BaseModel):
     id: str
     source: str
@@ -140,13 +23,6 @@ class PositionType(BaseModel):
     x: float
     y: float
 
-
-class NodeInfo(BaseModel):
-    entrezId: int
-    # label: str #symbol
-    name: str  # fullname
-    summary: str
-    synonyms: list[str]
 
 
 class Node(BaseModel):
@@ -164,76 +40,76 @@ class Gene2AllResponse(BaseModel):
     edges: list[Edge]
 
 
-@graph_router.get("/gene2genes")
-def gene2genes(gene: str | None = None, limit: int = 1000) -> list[Node] | None:
-    # df = graph_data
-    # if gene:
-    #     df = df[(df["ENSG_A"] == gene) & (df["ENSG_B"] != gene)]
-    # if not df.empty:
-    #     df = setGeneNamesFromGeneDf(df)
-    # return df.head(limit).to_dict(orient="records")# type: ignore
-    n = allNodes[allNodes["id"] == gene]
-    nDict = n.to_dict(orient="records")
-    return nDict
-
-
-@graph_router.get("/gene2diseases")
-def gene2diseases(gene: str | None = None, limit: int = 1000) -> list[TraitResponse]:
-    df = trait_data[~trait_data["disease"].str.contains("CHEBI")]
-    return gene2trait(df, gene, limit)
-
-
-@graph_router.get("/gene2drugs")
-def gene2drugs(gene: str | None = None, limit: int = 1000) -> list[TraitResponse]:
-    df = trait_data[trait_data["disease"].str.contains("CHEBI")]
-    return gene2trait(df, gene, limit)
-
-
-@graph_router.get("/trait2genes")
-def trait2genes(disease: str | None = None, limit: int = 1000) -> list[TraitResponse]:
-    df = trait_data
-    if disease:
-        df = df[df["disease"] == disease]
-    if not df.empty:
-        df = removeDuplicates(df)
-        df["gene_name"] = df["gene"].apply(getGeneName)
-    return df.head(limit).to_dict(orient="records")  # type: ignore
-
-
-@graph_router.get("/gene")
-def singleGene(gene: str) -> list[GeneResponse]:
-    df = graph_data.head(1).copy()
-    df.loc[0, "ENSG_A"] = gene
-    df.loc[0, "ENSG_B"] = gene
-    df = setGeneNamesFromGeneDf(df)
-    return df.to_dict(orient="records")  # type: ignore
-
-
-def gene2trait(
-    df: pd.DataFrame, gene: str | None = None, limit: int = 1000
-) -> list[TraitResponse]:
-    if gene:
-        df = df[df["gene"] == gene]
-    if not df.empty:
-        df = removeDuplicates(df)
-        df = setFromGeneName(df, True)
-    return df.head(limit).to_dict(orient="records")  # type: ignore
-
 
 def removeDuplicates(df: pd.DataFrame):
     df = df.sort_values(["disease", "padj"], ascending=[True, False])
-    df = df.drop_duplicates(subset=["gene", "disease"], keep="first")
+    df = df.drop_duplicates(subset=["disease"], keep="first")
     return df
 
 
-def setFromGeneName(df: pd.DataFrame, isTraitResponse=False):
-    ensg = df.iloc[0, 0]
-    # if isTraitResponse:
-    #     df["gene_name"] = getGeneName(ensg)
-    # else:
-    #     df["ENSG_A_name"] = getGeneName(ensg)
-    return df
+BASE_PATH = Path(__file__).parent
 
+# add file globals
+gene_names = pd.read_csv(BASE_PATH / "data/gwas_nodes.csv.gz", compression="gzip")
+graph_data = pd.read_csv(
+    BASE_PATH / "data/STRINGv11_OTAR281119_FILTER_combined.csv.gz", compression="gzip"
+)
+trait_data = pd.read_csv(
+    BASE_PATH / "data/gwas_gene-diseases.csv.gz", compression="gzip"
+)
+
+gene_list = (
+    pd.read_json(BASE_PATH / "data/gene_list.json")
+    .drop(columns=["hgncId", "hgncSymbol"], axis=1)
+    .rename(columns={"ensemblid": "id"})
+)
+
+gene_list = gene_list[gene_list["id"].notna()]
+gene_details = (
+    pd.read_json(BASE_PATH / "data/gene_descriptions.json")
+    .drop(columns=["fullname"], axis=1)
+    .rename(columns={"entrezGeneId": "entrezId"})
+)
+gene_nodes = pd.merge(gene_list, gene_details, on="entrezId")
+gene_nodes["type"] = "gene"
+gene_nodes = gene_nodes.fillna('NaN')
+
+trait_nodes = (removeDuplicates(pd.read_csv(BASE_PATH / "data/gwas_gene-diseases.csv.gz", compression="gzip"))
+    .drop(columns=["gene", "padj"])
+    .rename(columns={"disease": "id"}))
+
+trait_nodes["type"] = "disease"
+
+
+# TODO get trait names via API calls
+drug_nodes = trait_nodes[trait_nodes["id"].str.contains("CHEBI")].copy()
+drug_nodes["type"] = "drug"
+disease_nodes = trait_nodes[~trait_nodes["id"].str.contains("CHEBI")].copy()
+drug_nodes["synonyms"] = np.empty((len(drug_nodes), 0)).tolist()
+disease_nodes["synonyms"] = np.empty((len(disease_nodes), 0)).tolist()
+
+allNodes = pd.concat([gene_nodes, disease_nodes, drug_nodes], axis=0, ignore_index=True)
+
+
+@graph_router.get("/autocomplete")
+def autocomplete(search: str, limit: int | None = 10) -> list[str]:
+    #TODO implement nicer version (might look similar to the following line)
+    #df = allNodes[allNodes[(search in allNodes["id"]) | (search in allNodes["name"])| (search in allNodes["entrezId"])]]
+
+    ids = allNodes["id"].values.tolist()
+    names = allNodes["name"].values.tolist()
+    
+    results = []
+    
+    for ele in ids:
+        if search.lower() in str(ele).lower():
+            results.append(ele)
+    
+    for ele in names:
+        if  search.lower() in str(ele).lower():
+            results.append(str(ele))
+    
+    return results[:limit]
 
 @graph_router.get("/gene2all")
 def gene2all(gene: str | None = None, limit: int = 1000) -> Gene2AllResponse | None:
@@ -249,7 +125,7 @@ def gene2all(gene: str | None = None, limit: int = 1000) -> Gene2AllResponse | N
 
         # get traits
         df = trait_data
-        df = df[(df["gene"] == gene)]
+        df = df[(df["gene"] == gene) | (df["disease"] == gene)]
         df.rename(columns={"gene": "source", "disease": "target"}, inplace=True)
         df = df.drop("padj", axis=1)
         t = df.head(limit).to_dict(orient="records")
