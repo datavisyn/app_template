eae18012742875014ca4340f1258baba09b583a6
Faboi <fabio.baumgaertel@gmail.com>
2023-10-24 10:07:12 +0200

    commented some tests

--- app_template/graph_router.py
+++ app_template/graph_router.py
@@ -246,35 +246,35 @@ def setFromGeneName(df: pd.DataFrame, isTraitResponse=False):
 
 # additional trait (disease/drug) information
 
-@graph_router.get("/traitinfo/{trait_id}")
-def get_trait_info(trait_id: str):
-    name_info = get_diseaseOrDrug_name(trait_id)
-    # extraction of name and result
-    name = name_info["name"]
-    description = name_info["description"]
+# @graph_router.get("/traitinfo/{trait_id}")
+# def get_trait_info(trait_id: str):
+#     name_info = get_diseaseOrDrug_name(trait_id)
+#     # extraction of name and result
+#     name = name_info["name"]
+#     description = name_info["description"]
 
-    # create a response JSON with both name and description
-    response = {
-        "name": name,
-        "description": description
-    }
+#     # create a response JSON with both name and description
+#     response = {
+#         "name": name,
+#         "description": description
+#     }
 
-    return response
+#     return response
 
 
-# whole name for genes
+# # whole name for genes
 
-@graph_router.get("/geneinfo/{gene_id}")
-def get_gene(gene_id: str):
-    name = get_gene_name(gene_id)
-    gene_info = get_gene_info(gene_id)
+# @graph_router.get("/geneinfo/{gene_id}")
+# def get_gene(gene_id: str):
+#     name = get_gene_name(gene_id)
+#     gene_info = get_gene_info(gene_id)
     
-    # check whether gene was found
-    if isinstance(gene_info, str):
-        return gene_info
+#     # check whether gene was found
+#     if isinstance(gene_info, str):
+#         return gene_info
 
-    return {
-        "Gene Name": name,
-        "Transcript Product": gene_info.get("Transcript Product"),
-        "Chromosome Location": gene_info.get("Chromosome Location")
-    }
+#     return {
+#         "Gene Name": name,
+#         "Transcript Product": gene_info.get("Transcript Product"),
+#         "Chromosome Location": gene_info.get("Chromosome Location")
+#     }
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { Autocomplete, Loader, Button } from '@mantine/core';
-import React, { useState, useEffect, useCallback } from 'react';
+
+import { Autocomplete, Loader } from '@mantine/core';
+import React, { useState } from 'react';
 import { useAutocomplete, useGene2Genes } from './store/store';
 import { DrugGraph } from './DrugGraph';
 import { GeneGraph } from './GeneGraph';
@@ -13,32 +14,6 @@ export function App() {
     limit: 1000,
   });
 
-  const [geneInfo, setGeneInfo] = useState({});
-  const [showGeneInfo, setShowGeneInfo] = useState(false);
-
-  /* retrieve information for gene */
-  const fetchGeneInfo = useCallback(() => {
-    if (search) {
-      fetch(`http://localhost:9000/api/app/geneinfo/${search}`)
-        .then((response) => response.json())
-        .then((data) => {
-          setGeneInfo(data);
-        })
-        .catch((error) => {
-          console.error(error);
-          setGeneInfo({ 'Gene Name': 'Gene not found' });
-        });
-    }
-  }, [search]);
-
-  useEffect(() => {
-    fetchGeneInfo();
-  }, [fetchGeneInfo]);
-
-  const handleShowGeneInfo = () => {
-    setShowGeneInfo(true);
-  };
-
   return (
     <>
       <Autocomplete
@@ -50,26 +25,84 @@ export function App() {
         rightSection={isFetching ? <Loader size="sm" /> : null}
       />
 
-      <Button onClick={handleShowGeneInfo}>Show gene info</Button>
-
-      {showGeneInfo && (
-        <>
-          <GeneGraph geneID={search} />
-
-          {/* Display the gene info */}
-          <div>
-            {geneInfo && (
-              <div>
-                <strong>Gene Name:</strong> {geneInfo['Gene Name']}
-                <br />
-                <strong>Transcript Product:</strong> {geneInfo['Transcript Product']}
-                <br />
-                <strong>Chromosome Location:</strong> {geneInfo['Chromosome Location']}
-              </div>
-            )}
-          </div>
-        </>
-      )}
+      <GeneGraph geneID={search} />
     </>
   );
 }
+
+/* just for testing: */
+// import { Autocomplete, Loader, Button } from '@mantine/core';
+// import React, { useState, useEffect, useCallback } from 'react';
+// import { useAutocomplete, useGene2Genes } from './store/store';
+// import { DrugGraph } from './DrugGraph';
+// import { GeneGraph } from './GeneGraph';
+
+// export function App() {
+//   const [search, setSearch] = useState('');
+//   const { data: autocompleteData, isFetching } = useAutocomplete({ search });
+
+//   const { data: graph } = useGene2Genes({
+//     gene: search || undefined,
+//     limit: 1000,
+//   });
+
+//   const [geneInfo, setGeneInfo] = useState({});
+//   const [showGeneInfo, setShowGeneInfo] = useState(false);
+
+//   /* retrieve information for gene */
+//   const fetchGeneInfo = useCallback(() => {
+//     if (search) {
+//       fetch(`http://localhost:9000/api/app/geneinfo/${search}`)
+//         .then((response) => response.json())
+//         .then((data) => {
+//           setGeneInfo(data);
+//         })
+//         .catch((error) => {
+//           console.error(error);
+//           setGeneInfo({ 'Gene Name': 'Gene not found' });
+//         });
+//     }
+//   }, [search]);
+
+//   useEffect(() => {
+//     fetchGeneInfo();
+//   }, [fetchGeneInfo]);
+
+//   const handleShowGeneInfo = () => {
+//     setShowGeneInfo(true);
+//   };
+
+//   return (
+//     <>
+//       <Autocomplete
+//         label="Search for genes"
+//         placeholder="ENSG..."
+//         value={search}
+//         onChange={setSearch}
+//         data={autocompleteData || []}
+//         rightSection={isFetching ? <Loader size="sm" /> : null}
+//       />
+
+//       <Button onClick={handleShowGeneInfo}>Show gene info</Button>
+
+//       {showGeneInfo && (
+//         <>
+//           <GeneGraph geneID={search} />
+
+//           {/* Display the gene info */}
+//           <div>
+//             {geneInfo && (
+//               <div>
+//                 <strong>Gene Name:</strong> {geneInfo['Gene Name']}
+//                 <br />
+//                 <strong>Transcript Product:</strong> {geneInfo['Transcript Product']}
+//                 <br />
+//                 <strong>Chromosome Location:</strong> {geneInfo['Chromosome Location']}
+//               </div>
+//             )}
+//           </div>
+//         </>
+//       )}
+//     </>
+//   );
+// }
